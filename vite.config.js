70e1be9b64cccecfa77f5d4aa4295cfa7e7ccf79
Paul Ehrling <ghostlest330@gmail.com>
2025-06-09 11:54:49 +0200

    fix: correct asset paths in index.html and update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/barcode-scanner-website/',
+  base: '/Barcode-Scanner/',
   plugins: [vue()],
 })
